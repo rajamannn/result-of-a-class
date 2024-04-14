99968c8c143c76553b3ccf158223695f1cd6e0ad
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-04-14 17:34:22 +0530

    Add files via upload

---
+++ result of class.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int marks;
+    printf("Enter the marks");
+    scanf("%d",&marks);
+if(marks>=250)
+{
+    printf("Pass");
+}
+else
+    printf("Fail");
+getch();
+}
